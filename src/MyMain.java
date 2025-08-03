8cc9a55b07f0cfdc409dd8578b97b6af16936571
alina.sibil <alina.sibil@softswiss.com>
2025-08-03 11:44:05 +0200

    init commit

---
+++ src/MyMain.java
@@ -0,0 +1,23 @@
+//one line comment
+
+/*
+multiple line comment
+ */
+
+/**
+ * JavaDoc
+ */
+
+public class MyMain {
+    public static void main(String[] args){
+        System.out.print("Hello and welcome!");
+        System.out.println("2Hello and welcome!");
+        System.out.print("2Hello and welcome!");
+        //System.out.println(""); //просто добавляет пустую строку
+        System.out.println("   ");
+        System.out.print("Message");
+        System.out.print("\n"); //escape sequence
+        System.out.print("after message");
+
+}
+}
