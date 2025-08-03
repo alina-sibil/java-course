46fc80ddaa38b6defbdd08e33a077fd68350b5df
alina.sibil <alina.sibil@softswiss.com>
2025-08-03 20:50:25 +0200

    init commit

--- src/MyMain.java
+++ src/MyMain.java
@@ -9,7 +9,8 @@ multiple line comment
  */
 
 public class MyMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //виды комментов
         System.out.print("Hello and welcome!");
         System.out.println("2Hello and welcome!");
         System.out.print("2Hello and welcome!");
@@ -18,7 +19,25 @@ public class MyMain {
         System.out.print("Message");
         System.out.print("\n"); //escape sequence
         System.out.print("after message");
-        System.out.print("+new commit");
+        System.out.println("+new commit");
 
-}
+        // калькулятор с int
+        int a = 7, b = 3;
+        int ostatok = a % b;
+        System.out.println((a + b) / 2.0);
+        System.out.println("Остаток: " + ostatok);
+
+        //пример с boolean
+        int age = 17;
+        boolean isAdult = true;
+        boolean canDrinkAlcohol = isAdult && age >= 18;
+        System.out.println("Можно ли пить алкоголь? " + canDrinkAlcohol);
+
+        //просто пробую char
+        char letter = 'a';
+        char digit = '7';
+        char symbol = '#';
+        System.out.println("letter=" + letter + ", digit=" + digit + ", symbol=" + symbol);
+
+    }
 }
